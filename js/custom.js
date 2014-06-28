d095595fd8b93eff00540439b804cf80079b13f0
eanplatter <ean@eanplatter.com>
2014-06-27 23:36:18 -0600

    fresh

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 790) {
-        $(".navbar-fixed-bottom").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-bottom").removeClass("top-nav-collapse");
-    }
-});
