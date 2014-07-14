0bf70912ad771415f2d72fc10805e7e3f95d3884
eanplatter <ean@eanplatter.com>
2014-07-13 22:39:45 -0600

    round decimals and fix NaN issues

--- angular/app.js
+++ angular/app.js
@@ -1,12 +1,10 @@
-
-
-
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('DoubleController', ['$scope', function($scope) {
   	$scope.divider = function(value, rate) { 
-  		if(rate) {
-  			return value * (100 / 60) * rate / 100   			
+  		if(value && rate) {
+  			var total = value * (100 / 60) * rate / 100;
+  			return "$" + total.toFixed(2);  			
   		}
   	};
 }]);
